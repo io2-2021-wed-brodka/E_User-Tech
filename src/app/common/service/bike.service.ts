fdaf64e135d155a307dc4912f59c09680a0d5b34
stefano <99xkubax99@gmail.com>
2021-05-17 15:26:33 +0200

    fixed renting bikes display

--- src/app/common/service/bike.service.ts
+++ src/app/common/service/bike.service.ts
@@ -1,10 +1,8 @@
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {AppService} from "../../app.service";
-import {Router} from "@angular/router";
-import {Observable} from "rxjs";
-import {BikeDTO, BikeListDTO, BikeStationDTO} from "../../generated/dto";
-import {map} from "rxjs/operators";
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {BikeDTO, BikeListDTO} from '../../generated/dto';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class BikeService {
@@ -12,28 +10,30 @@ export class BikeService {
     constructor(private http: HttpClient) {
     }
 
-    getRentedBikes(): Observable<BikeDTO[]> {
-        return this.http.get<BikeDTO[]>("/api/bikes/rented");
+    getRentedBikes(): Observable<BikeListDTO> {
+        return this.http.get<BikeListDTO>('/api/bikes/rented');
     }
-    
+
     returnBike(id: number, stationId: number): Observable<any> {
 
         const body = {
-            "id": id
-        }
+            'id': id
+        };
 
         return this.http.post(`/api/stations/${stationId}/bikes`, body);
     }
 
-    rentBike(id: number) : Observable<any>{
+    rentBike(id: number): Observable<any> {
         const body = {
-            "id": id
-        }
-        return this.http.post("/api/bikes/rented", body);
+            'id': id
+        };
+        return this.http.post('/api/bikes/rented', body);
     }
 
     getBikesInStation(stationId: number): Observable<BikeDTO[]> {
         return this.http.get<BikeListDTO>(`/api/stations/${stationId}/bikes`)
             .pipe(map(r => r.bikes));
     }
+
+    deleteBike(bikeId: number):
 }
